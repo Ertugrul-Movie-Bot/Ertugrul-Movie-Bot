7ce8100333e725aa1fe954322ac2efb608b37a6f
AghinKhalidCP <86180193+AghinKhalidCP@users.noreply.github.com>
2022-01-11 07:13:44 +0530

    Bugs😬

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ logging.getLogger().setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
-from database.users_chats_db import db
+from ia_filterdb import Media
+from users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN
 from utils import temp
 
